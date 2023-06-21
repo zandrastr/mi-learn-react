ab0d5a7d88f6899a13dede0e68cd462bc7f4282c
Sandra S <zandrastr@hotmail.com>
2023-06-21 16:58:50 +0200

    part 2 - first commit

---
+++ 2-rendering/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { ObjectPresentation } from './components/ObjectPresentation';
+import { ObjectsPresentation } from './components/ObjectsPresentation';
+
+function App() {
+    return (
+        <>
+            <ObjectPresentation></ObjectPresentation>
+            <ObjectsPresentation></ObjectsPresentation>
+        </>
+    );
+}
+
+export default App;
