0d8811eec5f9b92cb6a05c38d6ef0d5fd6d66d53
Sandra S <zandrastr@hotmail.com>
2023-07-05 15:25:09 +0200

    add dog class

---
+++ 3-state/src/models/Dog.ts
@@ -0,0 +1,8 @@
+export class Dog {
+    constructor(
+        public name: string, 
+        public age: number, 
+        public color: string, 
+        public isVaccinated: boolean) 
+        {}
+}
