c5ef382534ee7426012f639620c700c6511e316f
Sandra S <zandrastr@hotmail.com>
2023-07-08 18:05:11 +0200

    add handle toggle in todo app

--- 4-props/src/components/TodoApp.tsx
+++ 4-props/src/components/TodoApp.tsx
@@ -6,10 +6,24 @@ import { Todos } from './Todos';
 export const TodoApp = () => {
   const [todos, setTodos] = useState<Todo[]>([new Todo('A task', false, 1), new Todo('Another task', false, 2)]);
 
+  const handleToggle = (id: number) => {
+    console.log('Do toggle in TodoApp, id:', id);
+
+    setTodos(
+      todos.map((oneTodo) => {
+        if (oneTodo.id === id) {
+          return { ...oneTodo, done: !oneTodo.done };
+        } else {
+          return oneTodo;
+        }
+      })
+    );
+  };
+
   return (
     <>
       <AddTodo></AddTodo>
-      <Todos listOfTodos={todos}></Todos>
+      <Todos listOfTodos={todos} toggle={handleToggle}></Todos>
     </>
   );
 };
