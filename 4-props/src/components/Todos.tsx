b8c329983fcee8a6e7d0be382d9e84ac5b01dfe4
Sandra S <zandrastr@hotmail.com>
2023-07-08 18:06:03 +0200

    update todos component

--- 4-props/src/components/Todos.tsx
+++ 4-props/src/components/Todos.tsx
@@ -1,11 +1,18 @@
 import { Todo } from '../models/Todo';
+import { TodoView } from './TodoView';
 
 interface ITodosProps {
   listOfTodos: Todo[];
+  toggle: (id: number) => void;
 }
 
-export const Todos = ({ listOfTodos }: ITodosProps) => {
-  const html = listOfTodos.map((oneTodo) => <li key={oneTodo.id}>{oneTodo.text}</li>);
+export const Todos = ({ listOfTodos, toggle }: ITodosProps) => {
+  const handleToggle = (id: number) => {
+    console.log('Handle toggle in Todos, id:', id);
+    toggle(id);
+  };
 
-  return <ul>{html}</ul>;
+  const html = listOfTodos.map((oneTodo) => <TodoView todo={oneTodo} key={oneTodo.id} toggle={handleToggle}></TodoView>);
+
+  return <div>{html}</div>;
 };
