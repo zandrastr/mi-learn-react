e03b0e96cbe5c6febc12f8670f47b6632c23008e
Sandra S <zandrastr@hotmail.com>
2023-07-15 20:04:38 +0200

    add useloaderdata to display movies

--- 5-router/src/components/Movies.tsx
+++ 5-router/src/components/Movies.tsx
@@ -1,3 +1,18 @@
+import { useLoaderData } from 'react-router-dom';
+import { IMoviesLoader } from '../loaders/moviesLoader';
+
 export const Movies = () => {
-  return <>Movies</>;
+  const { movies } = useLoaderData() as IMoviesLoader;
+
+  return (
+    <>
+      {movies.map((movie) => {
+        return (
+          <div>
+            <h3>{movie.Title}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
 };
