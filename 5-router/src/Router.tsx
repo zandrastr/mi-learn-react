45860c96d880343e5ea7c8d050be97ef90de812c
Sandra S <zandrastr@hotmail.com>
2023-07-15 16:15:10 +0200

    add layout component and update router

--- 5-router/src/Router.tsx
+++ 5-router/src/Router.tsx
@@ -2,18 +2,26 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Movies } from './components/Movies';
 import { Movie } from './components/Movie';
+import { Layout } from './components/Layout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>,
-  },
-  {
-    path: '/movies',
-    element: <Movies></Movies>,
-  },
-  {
-    path: '/movie/:id',
-    element: <Movie></Movie>,
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        index: true,
+      },
+      {
+        path: '/movies',
+        element: <Movies></Movies>,
+      },
+      {
+        path: '/movie/:id',
+        element: <Movie></Movie>,
+      },
+    ],
   },
 ]);
