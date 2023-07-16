fb8b92590ad4e8a2713831dafaeeaf2590c9c57c
Sandra S <zandrastr@hotmail.com>
2023-07-16 18:02:28 +0200

    add usenavigate hook, alt way to nav to read more

--- 5-router/src/components/Movies.tsx
+++ 5-router/src/components/Movies.tsx
@@ -1,16 +1,21 @@
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import { IMoviesLoader } from '../loaders/moviesLoader';
 
 export const Movies = () => {
   const { movies } = useLoaderData() as IMoviesLoader;
+  const navigate = useNavigate();
+
+  const handleClick = (id: string) => {
+    navigate('/movie/' + id);
+  };
 
   return (
     <>
       {movies.map((movie) => {
         return (
-          <div>
+          <div onClick={() => handleClick(movie.imdbID)} key={movie.imdbID}>
             <h3>{movie.Title}</h3>
-            <Link to={'/movie/' + movie.imdbID}>Read more...</Link>
+            {/* <Link to={'/movie/' + movie.imdbID}>Read more...</Link> */}
           </div>
         );
       })}
