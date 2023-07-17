e67b98c646bcdf365080ca8ff579eba6e4afeb2f
Sandra S <zandrastr@hotmail.com>
2023-07-17 19:34:09 +0200

    create movie api call and display data

--- 5-router/src/components/Movie.tsx
+++ 5-router/src/components/Movie.tsx
@@ -1,3 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IMovie } from '../models/IMovie';
+
 export const Movie = () => {
-  return <>Movie</>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState<IMovie>();
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const response = await axios.get<IMovie>('https://www.omdbapi.com/?apikey=9508b94c&i=' + (id ?? ''));
+
+      setMovie(response.data);
+    };
+
+    if (!movie) void getMovie();
+  }, [movie, id]);
+
+  return (
+    <>
+      <h2>{movie?.Title}</h2>
+      <img src={movie?.Poster} alt={movie?.Title} />
+    </>
+  );
 };
